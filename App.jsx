9fa7561cf6a268d91b36ade437f54a87d91cbaea
Charles Wafula <charleswafula73@gmail.com>
2024-07-16 17:18:32 +0300

    add statusbar

--- App.jsx
+++ App.jsx
@@ -1,17 +1,22 @@
 import React, { useState } from 'react';
-import { View, ScrollView, Text, Image, Button, Modal } from 'react-native';
+import { View, ScrollView, Text, Image, Button, Modal, StatusBar, ActivityIndicator } from 'react-native';
 import logoImage from './assets/adaptive-icon.png'
 
 export default function App() {
   const [modal, setModal] = useState(false)
+  const [isVisibleBar, setIsVisibleBar] = useState(true)
   return (
     <View style={{ flex: 1, padding: 25, backgroundColor: 'plum' }}>
+      <ActivityIndicator/>
+      <StatusBar backgroundColor={'green'} barStyle='dark-content' hidden={isVisibleBar} />
       <ScrollView>
         <Text>
           The standard Lorem Ipsum passage, used since the 1500s
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis dignissimos dolor debitis explicabo corporis sint veniam eum quo cupiditate ab veritatis, libero temporibus? A odio molestiae, fuga quasi ut nostrum!z Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, autem corporis fugiat doloremque dignissimos ratione eum mollitia asperiores cupiditate ea, nihil esse. Rem, aliquam molestias! Pariatur veniam enim placeat quod!Lorem Lorem ipsum, dolor sit amet consectetur adipisicing elit. A deleniti in tempora animi necessitatibus inventore cumque architecto. Fugit amet harum tempora, nobis dolorem ipsum recusandae, expedita necessitatibus consequuntur, deserunt dignissimos.
         </Text>
-        <Button title='Press' color='green' onPress={() => setModal(true)} />
+        {/* <Button title='Press' color='green' onPress={() => setModal(true)} /> */}
+        <Button title='Show Status Bar' color='green' onPress={() => setIsVisibleBar(!isVisibleBar)} />
+
 
         <Modal visible={modal} onRequestClose={() => setModal(false)} animationType='slide' presentationStyle='pageSheet'>
           <View >
